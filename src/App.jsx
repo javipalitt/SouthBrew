57a54cf6dc2666a336f245c12ee6cfb2c0189560
javipalitt <palitt25@gmail.com>
2020-12-03 21:54:25 -0300

    Cambios al día de la fecha
    Eliminación de datos innecesarios y agregamos alguna información nueva.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import Hero from './components/Home/Hero';
+
+function App() {
+  return (
+    <>
+<Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <>
+    <h1 className="titulo-principal"> Hola mundo! </h1>
+    <p className="subtitulo">Este es mi e-commerce</p>
+    </>
+  );
+}
+
+export default Hero;
